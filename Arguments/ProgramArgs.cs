53bab315382c5bd8e61c5e84720cd289b7c2cb57
Patryk Wasiewicz <pat.wasiewicz@gmail.com>
2015-01-06 14:06:20 +0100

    Program args base

---
+++ Arguments/ProgramArgs.cs
@@ -0,0 +1,10 @@
+﻿namespace KnapsackProblem.Arguments
+{
+    using CommandLine;
+
+    public class ProgramArgs
+    {
+        [Option('w', "wait", DefaultValue = false)]
+        public bool WaitForKey { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,9 +1,37 @@
 ﻿namespace KnapsackProblem
 {
+    using CommandLine;
+    using KnapsackProblem.Arguments;
+    using System;
+
     public class Program
     {
+
         public static void Main(string[] args)
         {
+            var programArgs = new ProgramArgs();
+            var parser = new Parser(settings =>
+                                    {
+                                        settings.HelpWriter = null;
+                                        settings.IgnoreUnknownArguments = false;
+                                        settings.MutuallyExclusive = true;
+                                    });
+
+            parser.ParseArgumentsStrict(args, programArgs, () =>
+                                                           {
+                                                               WriteLine("Program ran with invalid arguments.");
+                                                               Environment.Exit(0);
+                                                           });
+
+            if (programArgs.WaitForKey)
+            {
+                Console.ReadKey();
+            }
+        }
+
+        public static void WriteLine(string format, params object[] args)
+        {
+            Console.Out.WriteLine(format, args);
         }
     }
 }
