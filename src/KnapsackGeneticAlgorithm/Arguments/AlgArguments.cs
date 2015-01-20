5d5b19b5f28fab3f83cc854a651f7b090865fcb7
Patryk Wasiewicz <pat.wasiewicz@gmail.com>
2015-01-20 22:52:02 +0100

    Arguments help text

--- src/KnapsackGeneticAlgorithm/Arguments/AlgArguments.cs
+++ src/KnapsackGeneticAlgorithm/Arguments/AlgArguments.cs
@@ -4,13 +4,13 @@
 
     internal class AlgArguments
     {
-        [Option('g', "generations", DefaultValue = 1000)]
+        [Option('g', "generations", DefaultValue = 1000, HelpText = "Number of generations.")]
         public int Generations { get; set; }
 
-        [Option('p', "population", DefaultValue = 100)]
+        [Option('p', "population", DefaultValue = 100, HelpText = "Number of chromosomes in one population.")]
         public int Population { get; set; }
 
-        [Option('m', "mutation", DefaultValue = 0.1)]
+        [Option('m', "mutation", DefaultValue = 0.1, HelpText = "Probability of child chromosome mutation.")]
         public double MutationProbability { get; set; }
 
         [Option("force-generations", DefaultValue = false, HelpText = "Forces to use all generations to search solution. Otherwise, when 75% has the same fitness - algorithm is ended.")]
--- src/TabuAlgorithm/Arguments/AlgArguments.cs
+++ src/TabuAlgorithm/Arguments/AlgArguments.cs
@@ -4,16 +4,16 @@
 
     internal class AlgArguments
     {
-        [Option('t', "tabu", DefaultValue = 150)]
+        [Option('t', "tabu", DefaultValue = 150, HelpText = "Maximum size of tabu search list.")]
         public int TabuSize { get; set; }
 
-        [Option('i', "iterations", DefaultValue = 3000)]
+        [Option('i', "iterations", DefaultValue = 3000, HelpText = "Number of iterations.")]
         public int Iterations { get; set; }
 
-        [Option('n', "neighbourhoods", DefaultValue = 200)]
+        [Option('n', "neighbourhoods", DefaultValue = 200, HelpText = "Quantity of neighbourhoods generated for best solution.")]
         public int Neighbourhoods { get; set; }
 
-        [Option('s', "selection", DefaultValue = "swap")]
+        [Option('s', "selection", DefaultValue = "swap", HelpText = "Selection algorithm name.")]
         public string Selection { get; set; }
     }
 }
